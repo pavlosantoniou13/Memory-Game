b70d6af7a02bd3bd4d9fc4f7764aba40c50b7bb8
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-10 21:48:11 +0200

    added images showing from the object

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import Navbar from "./Components/Navbar";
 import './styles.css'
 import CardsContainer from "./Components/CardsContainer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import morty from './Components/images/morty.jpg'
+import rick from './Components/images/rick.webp'
+import jerry from './Components/images/jerry.jpg'
+import beth from './Components/images/beth.webp'
+import birdperson from './Components/images/birdperson.jpg'
+import saul from './Components/images/saul.webp'
+import waltuh from './Components/images/walter.webp'
+import jesse from './Components/images/jesse.webp'
+import kid from './Components/images/kid.jpg'
+import tsira from './Components/images/tsira.webp'
 
 const cardsObj = [
-  { "src": './Components/images/morty.jpg' },
-  { "src": './Components/images/rick.webp' },
-  { "src": './Components/images/jerry.jpg' },
-  { "src": './Components/images/beth.jpg' },
-  { "src": './Components/images/kid.jpg'},
-  { "src": './Components/images/saul.webp'},
-  { "src": './Components/images/walter.webp'},
-  { "src": './Components/images/tsira.webp'},
-  { "src": './Components/images/birdperson.jpg'},
-  { "src": './Components/images/jesse.webp'},
+  { name: morty, Name: "Morty" },
+  { name: rick, Name: "Rick" },
+  { name: jerry, Name: "Jerry" },
+  { name: birdperson, Name: "Birdperson" },
+  { name: saul, Name: "Saul" },
+  { name: beth, Name: "Beth" },
+  { name: waltuh, Name: "Waltuh" },
+  { name: jesse, Name: "Jesse" },
+  { name: tsira, Name: "Tsira" },
+  { name: kid, Name: "Kid" },
+  
 ]
  
 function App() {
@@ -27,15 +38,30 @@ function App() {
 
     setCards(shuffledcards)
     
-    
   }
+
+  const counter = (e) => {
+    if(e.event.target.card.isClicked === false) {
+      console.log("h karta den eixe patithei")
+    } else if(e.event.target.isClicked === false) {
+      console.log("h karta eixe patithei")
+    }
+  }
+
 
   console.log(cards)
   
+  useEffect(() => {
+    let ignore = false;
+    
+    if (!ignore)  shuffleCards()
+    return () => { ignore = true; }
+    },[]);
+
   return (
     <div>
       <Navbar  />
-      <CardsContainer shuffleCards={shuffleCards} cards={cards} />
+      <CardsContainer shuffleCards={shuffleCards} cards={cards} counter={counter} />
     </div>
     
   );
--- src/Components/CardsContainer.js
+++ src/Components/CardsContainer.js
@@ -1,93 +1,26 @@
 import React from 'react'
-import img1 from './images/img1.jpg'
-import morty from './images/morty.jpg'
-import rick from './images/rick.webp'
-import jerry from './images/jerry.jpg'
-import beth from './images/beth.webp'
-import birdperson from './images/birdperson.jpg'
-import saul from './images/saul.webp'
-import waltuh from './images/walter.webp'
-import jesse from './images/jesse.webp'
-import kid from './images/kid.jpg'
-import tsira from './images/tsira.webp'
-
-
-
-
-
-
 
 
 export default function CardsContainer(props) {
 
-    function printCards() {
-        for( let i = 0; i < props.cards.length; i++) {
-            console.log(props.cards[i])
-        }
-    }
+    
+   
 
   return (
     <div className="container">
+        
         <div className="box-container">
+
+          
+            {props.cards.map(card => (
+                <div className="box" key={card.id}  onClick={props.shuffleCards}  >
+                     <img onClick={props.counter} src={card.name} alt="" />
+                     <h3>{card.Name}</h3>
+                </div>
+            ))}
             
-            <div className="box"  onClick={props.shuffleCards} >
-                {props.cards.map(card => (
-                    <div key={card.id}  >
-                        <img src={card.src} alt="" />
-                        <h3>Name of whatever</h3>
-                    </div>
-                ))}
-            </div>
-
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={rick}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={morty}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={jerry}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards}  >
-                <img src={beth}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
+                        
             
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={birdperson}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={saul}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={waltuh}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards}  >
-                <img src={jesse}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={kid}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
-
-            <div className="box" onClick={props.shuffleCards} >
-                <img src={tsira}alt="" />
-                <h3>Name of whatever</h3>
-            </div>
         </div>
         
     </div>
