5b3ca22cc50075e6461685949a48e076eb3cdd31
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-10 17:00:22 +0200

    working on making the cards shuffle and display, managed to make them shuffle but the src doesnt display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import Navbar from "./Components/Navbar";
 import './styles.css'
 import CardsContainer from "./Components/CardsContainer";
+import { useState } from "react";
+
+const cardsObj = [
+  { "src": './Components/images/morty.jpg' },
+  { "src": './Components/images/rick.webp' },
+  { "src": './Components/images/jerry.jpg' },
+  { "src": './Components/images/beth.jpg' },
+  { "src": './Components/images/kid.jpg'},
+  { "src": './Components/images/saul.webp'},
+  { "src": './Components/images/walter.webp'},
+  { "src": './Components/images/tsira.webp'},
+  { "src": './Components/images/birdperson.jpg'},
+  { "src": './Components/images/jesse.webp'},
+]
  
 function App() {
+
+  const [cards, setCards] = useState([])
+
+  const shuffleCards = () => {
+   const shuffledcards = [...cardsObj]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random(), isClicked:false }))
+
+    setCards(shuffledcards)
+    
+    
+  }
+
+  console.log(cards)
+  
   return (
     <div>
-      <Navbar />
-    <CardsContainer />
+      <Navbar  />
+      <CardsContainer shuffleCards={shuffleCards} cards={cards} />
     </div>
     
   );
--- src/Components/CardsContainer.js
+++ src/Components/CardsContainer.js
@@ -18,56 +18,73 @@ import tsira from './images/tsira.webp'
 
 
 
-export default function CardsContainer() {
+export default function CardsContainer(props) {
+
+    function printCards() {
+        for( let i = 0; i < props.cards.length; i++) {
+            console.log(props.cards[i])
+        }
+    }
+
   return (
     <div className="container">
         <div className="box-container">
-            <div className="box">
+            
+            <div className="box"  onClick={props.shuffleCards} >
+                {props.cards.map(card => (
+                    <div key={card.id}  >
+                        <img src={card.src} alt="" />
+                        <h3>Name of whatever</h3>
+                    </div>
+                ))}
+            </div>
+
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={rick}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={morty}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={jerry}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards}  >
                 <img src={beth}alt="" />
                 <h3>Name of whatever</h3>
             </div>
             
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={birdperson}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={saul}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={waltuh}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards}  >
                 <img src={jesse}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={kid}alt="" />
                 <h3>Name of whatever</h3>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={props.shuffleCards} >
                 <img src={tsira}alt="" />
                 <h3>Name of whatever</h3>
             </div>
